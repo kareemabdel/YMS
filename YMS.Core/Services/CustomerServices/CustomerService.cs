8d3701111f3e2dd2fe9e780c89803be049960d97
Kareem Slama <kareem.slama@crayon.com>
2024-10-15 12:56:30 +0300

    push customer controller and services

---
+++ YMS.Core/Services/CustomerServices/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YMS.Core.Models.Customers;
+using YMS.Core.Models;
+using YMS.Core.Models.Users;
+using YMS.Migrations.Entities;
+using YMS.Migrations.UnitOfWorks;
+using YMS.Core.Models.Filters;
+
+namespace YMS.Core.Services.UserServices
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CustomerService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public Task<ApiResponse<PaginatedList<CustomerListDto>>> GetAll(CustomerFilter? filter, int page, int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YMS.Web/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YMS.Core.Models;
+using YMS.Core.Models.AuthenticationModels;
+using YMS.Core.Models.Customers;
+using YMS.Core.Models.Filters;
+using YMS.Core.Services.AuthenticationService;
+using YMS.Core.Services.UserServices;
+
+namespace YMS.Web.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CustomerController : BaseController
+    {
+        private readonly ICustomerService _customerervice;
+        public CustomerController(ICustomerService customerervice)
+        {
+            _customerervice = customerervice;
+        }
+
+        [HttpPost("GetAll")]
+        public async Task<IActionResult> GetAll([FromQuery] CustomerFilter? filter, int page = 1, int size = 10)
+        {
+            var response = await _customerervice.GetAll(filter,page,size);
+            return GetAPIResponse(response);
+        }     
+    }
+}
