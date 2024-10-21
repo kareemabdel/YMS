bc9427d5a423c7bd7b96b4507700aafd3e1f12fa
Kareem Slama <kareem.slama@crayon.com>
2024-10-21 16:05:12 +0300

    comitt

--- YMS.Migrations/Entities/ContainerTransaction.cs
+++ YMS.Migrations/Entities/ContainerTransaction.cs
@@ -17,22 +17,21 @@ namespace YMS.Migrations.Entities
         public Guid CustomerId { get; set; }
         public int? LineId { get; set; }
         public int TransporterId { get; set; }
-        public DateTime GateInDate { get; set; }
         public string TruckNo { get; set; }
-        public string DriverMobileNumber { get; set; }
-        public string DeliveryCardNo { get; set; }
-        public string DriverID { get; set; }
-        public decimal CleanCost { get; set; }
-        public decimal RepairCost { get; set; }
-        public int BlockId { get; set; }
+        public string? DriverMobileNumber { get; set; }
+        public string? DeliveryCardNo { get; set; }
+        public string? DriverID { get; set; }
+        public decimal? CleanCost { get; set; }
+        public decimal? RepairCost { get; set; }
+        public int? BlockId { get; set; }
         [Range(10, 99, ErrorMessage = "The Bay value must be a two-digit integer.")]
-        public short Bay { get; set; }
+        public short? Bay { get; set; }
 
         [Range(10, 99, ErrorMessage = "The Rows value must be a two-digit integer.")]
-        public short Rows { get; set; }
+        public short? Rows { get; set; }
 
         [Range(0, 9, ErrorMessage = "The Tier value must be a one-digit integer.")]
-        public short Tier { get; set; }
+        public short? Tier { get; set; }
         public int Status { get; set; } //ContainerStatusEnum
 
         /// <summary>
